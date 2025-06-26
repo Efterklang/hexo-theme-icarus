f865a69edca82deb36cd7bc9b3d64d3fbafcd3c7
Efterklang <gaojiaxing2004@qq.com>
2025-06-27 03:15:26 +0800

    feat(toc)!: implement floating table of contents with styling and visibility controls

--- layout/common/widgets.jsx
+++ layout/common/widgets.jsx
@@ -1,5 +1,35 @@
 const { Component, view, classname, logger, loadComponent, handleWidgetError } = require('../../include/util/common');
 
+class FloatingToc extends Component {
+    render() {
+        const { helper, page, config } = this.props;
+        const tocContent = helper.toc(page.content, {
+            class: 'menu-list',
+            list_number: false
+        });
+
+        if (!tocContent) {
+            return null;
+        }
+
+        return (
+            <div class="floating-toc-container">
+                <div class="floating-toc-button">
+                    <i class="fas fa-list-ul"></i>
+                </div>
+                <div class="card widget">
+                    <div class="card-content">
+                        <div class="menu">
+                            <h3 class="menu-label">{helper.__('widget.catalogue')}</h3>
+                            <div dangerouslySetInnerHTML={{ __html: tocContent }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 function formatWidgets(widgets) {
     const result = {};
     if (Array.isArray(widgets)) {
@@ -17,10 +47,10 @@ function formatWidgets(widgets) {
 }
 
 function hasColumn(widgets, position, config, page) {
-    const showToc = (config.toc === true) && ['page', 'post'].includes(page.layout);
     if (Array.isArray(widgets)) {
         return typeof widgets.find(widget => {
-            if (widget.type === 'toc' && !showToc) {
+            // Exclude TOC from column calculation
+            if (widget.type === 'toc') {
                 return false;
             }
             return widget.position === position;
@@ -36,7 +66,6 @@ function getColumnCount(widgets, config, page) {
 function getColumnSizeClass(columnCount) {
     switch (columnCount) {
         case 2:
-            // return 'is-4-tablet is-4-desktop is-4-widescreen';
             return 'is-4-tablet is-3-desktop is-3-widescreen';
         case 3:
             return 'is-4-tablet is-4-desktop is-3-widescreen';
@@ -66,37 +95,43 @@ class Widgets extends Component {
         const { site, config, helper, page, position } = this.props;
         const widgets = formatWidgets(config.widgets)[position] || [];
         const columnCount = getColumnCount(config.widgets, config, page);
+        const showToc = (config.toc === true) && ['page', 'post'].includes(page.layout) && helper.toc(page.content);
 
-        if (!widgets.length) {
+        const columnWidgets = widgets.filter(widget => widget.type !== 'toc');
+
+        if (!columnWidgets.length && !(position === 'left' && showToc)) {
             return null;
         }
 
-        return <div class={classname({
-            'column': true,
-            ['column-' + position]: true,
-            [getColumnSizeClass(columnCount)]: true,
-            [getColumnVisibilityClass(columnCount, position)]: true,
-            [getColumnOrderClass(position)]: true,
-            'is-sticky': isColumnSticky(config, position)
-        })}>
-            {widgets.map(widget => {
-                // widget type is not defined
-                if (!widget.type) {
-                    return null;
-                }
-                const Widget = loadComponent('widget/' + widget.type);
-                if (!Widget) {
-                    handleWidgetError(widget.type);
-                    return null;
-                }
-                return <Widget site={site} helper={helper} config={config} page={page} widget={widget} />;
-            })}
-            {position === 'left' && hasColumn(config.widgets, 'right', config, page) ? <div class={classname({
-                'column-right-shadow': true,
-                'is-hidden-widescreen': true,
-                'is-sticky': isColumnSticky(config, 'right')
-            })}></div> : null}
-        </div>;
+        return <>
+            {position === 'left' && showToc ? <FloatingToc {...this.props} /> : null}
+
+            {columnWidgets.length ? <div class={classname({
+                'column': true,
+                ['column-' + position]: true,
+                [getColumnSizeClass(columnCount)]: true,
+                [getColumnVisibilityClass(columnCount, position)]: true,
+                [getColumnOrderClass(position)]: true,
+                'is-sticky': isColumnSticky(config, position)
+            })}>
+                {columnWidgets.map(widget => {
+                    if (!widget.type) {
+                        return null;
+                    }
+                    const Widget = loadComponent('widget/' + widget.type);
+                    if (!Widget) {
+                        handleWidgetError(widget.type);
+                        return null;
+                    }
+                    return <Widget site={site} helper={helper} config={config} page={page} widget={widget} />;
+                })}
+                {position === 'left' && hasColumn(config.widgets, 'right', config, page) ? <div class={classname({
+                    'column-right-shadow': true,
+                    'is-hidden-widescreen': true,
+                    'is-sticky': isColumnSticky(config, 'right')
+                })}></div> : null}
+            </div> : null}
+        </>;
     }
 }
 
