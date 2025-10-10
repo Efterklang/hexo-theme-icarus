28d4fe74b06439c1533657eb5233f546a756c192
Efterklang <gaojiaxing2004@qq.com>
2025-10-10 17:10:27 +0800

    fix(error): The 'viewport' meta element 'content' attribute value should not contain 'maximum-scale'.

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -122,10 +122,7 @@ module.exports = class extends Component {
     return (
       <head>
         <meta charset="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=5"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {noIndex ? <meta name="robots" content="noindex" /> : null}
         {meta && meta.length ? <MetaTags meta={meta} /> : null}
         <title>{getPageTitle(page, config.title, helper)}</title>
