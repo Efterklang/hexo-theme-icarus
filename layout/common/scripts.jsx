46eabe97451d2e09fc438a9fe243cbf8b142b09f
Efterklang <gaojiaxing2004@qq.com>
2025-11-15 16:04:46 +0800

    fix: moment is not defined error

--- layout/common/scripts.jsx
+++ layout/common/scripts.jsx
@@ -10,9 +10,8 @@ module.exports = class extends Component {
 
         return <Fragment>
             <script defer src={cdn('jquery', '3.3.1', 'dist/jquery.min.js')}></script>
-            <script defer src={cdn('moment', '2.22.2', 'min/moment-with-locales.min.js')}></script>
+            <script defer src={cdn('moment', '2.22.2', 'min/moment-with-locales.min.js')} onload={`moment.locale("${language}");`}></script>
             {config.comment?.js_url && <script defer src={config.comment.js_url}></script>}
-            <script dangerouslySetInnerHTML={{ __html: `moment.locale("${language}");` }}></script>
             <script async data-pjax src={url_for('/js/column.js')}></script>
             <Plugins site={site} config={config} page={page} helper={helper} head={false} />
             <script defer data-pjax src={url_for('/js/main.js')} ></script>
