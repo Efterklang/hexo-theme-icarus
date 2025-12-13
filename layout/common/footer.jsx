4cd2b922d83bdab828de321c9a54f7bfac96eb45
Efterklang <gaojiaxing2004@qq.com>
2025-12-13 19:58:04 +0800

    refactor layout & footer

--- layout/common/footer.jsx
+++ layout/common/footer.jsx
@@ -14,53 +14,72 @@ class Footer extends Component {
             visitorCounterTitle,
             ICPRecord
         } = this.props;
-        return <footer class="footer">
-            <div class="container">
-                <div class="footer-grid">
-                    <div class="footer-column footer-brand">
-                        <p class="footer-title">{author || siteTitle}</p>
-                        <p class="footer-meta is-size-7">
-                            <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
-                            &nbsp;Powered by Hexo&nbsp;& <a href="https://github.com/Efterklang/hexo-theme-icarus" target="_blank" rel="noopener">Icarus</a>
-                            {showVisitorCounter ? <><br /><span id="busuanzi_container_site_uv" dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span></> : null}
-                            {/* 备案 */}
-                            {ICPRecord ? <><br /><a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener" dangerouslySetInnerHTML={{ __html: ICPRecord }}></a></> : null}
-                        </p>
-                        {copyright ? <p class="footer-meta" dangerouslySetInnerHTML={{ __html: copyright }}></p> : null}
-                    </div>
-                    <div class="footer-column footer-social">
-                        <p class="footer-heading">Social Media</p>
-                        <div class="footer-links">
-                            {Object.keys(links).length ? Object.keys(links).map(name => {
-                                const link = links[name];
-                                return <a class="footer-link" target="_blank" rel="noopener" title={name} href={link.url}>
-                                    {link.icon ? <iconify-icon icon={link.icon}></iconify-icon> : name}
-                                </a>;
-                            }) : null}
-                        </div>
-                    </div>
-                    <div class="footer-column footer-subdomains">
-                        <p class="footer-heading">Sub Domains</p>
-                        <div class="footer-links">
-                            {Object.keys(subdomains).length ? Object.keys(subdomains).map(name => {
-                                const link = subdomains[name];
-                                return <a class="footer-link" target="_blank" rel="noopener" title={name} href={link.url}>{name}</a>;
-                            }) : null}
-                        </div>
-                    </div>
-                    <div class="footer-column footer-archives">
-                        <p class="footer-heading">Archives</p>
-                        <div class="footer-archive-list">
-                            {archives && archives.length ? archives.map(item => (
-                                <span class="footer-archive-pill">
-                                    <a class="footer-link" href={item.url}>{item.year}</a>
-                                </span>
-                            )) : null}
-                        </div>
+
+        const svg_line = <svg aria-hidden="true" width="100%" height="8" fill="none" xmlns="http://www.w3.org/2000/svg" >
+            <pattern id="a" width="91" height="8" patternUnits="userSpaceOnUse" >
+                <g clip-path="url(#clip0_2426_11367)">
+                    <path d="M114 4c-5.067 4.667-10.133 4.667-15.2 0S88.667-.667 83.6 4 73.467 8.667 68.4 4 58.267-.667 53.2 4 43.067 8.667 38 4 27.867-.667 22.8 4 12.667 8.667 7.6 4-2.533-.667-7.6 4s-10.133 4.667-15.2 0S-32.933-.667-38 4s-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0" stroke="var(--lavender)" stroke-linecap="square" ></path>
+                </g>
+            </pattern>
+            <rect width="100%" height="100%" fill="url(#a)" ></rect>
+        </svg>
+
+        const footer_brand = <div class="footer-column footer-brand">
+            <p class="footer-title">{author || siteTitle}</p>
+            <p class="footer-meta is-size-7">
+                <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
+                &nbsp;Powered by Hexo&nbsp;& <a href="https://github.com/Efterklang/hexo-theme-icarus" target="_blank" rel="noopener">Icarus</a>
+                {showVisitorCounter ? <><br /><span id="busuanzi_container_site_uv" dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span></> : null}
+                {/* 备案 */}
+                {ICPRecord ? <><br /><a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener" dangerouslySetInnerHTML={{ __html: ICPRecord }}></a></> : null}
+            </p>
+            {copyright ? <p class="footer-meta" dangerouslySetInnerHTML={{ __html: copyright }}></p> : null}
+        </div>
+
+        const footer_social = <div class="footer-column footer-social">
+            <p class="footer-heading">Social Media</p>
+            <div class="footer-links">
+                {Object.keys(links).length ? Object.keys(links).map(name => {
+                    const link = links[name];
+                    return <a class="footer-link" target="_blank" rel="noopener" title={name} href={link.url}>
+                        {link.icon ? <iconify-icon icon={link.icon}></iconify-icon> : name}
+                    </a>;
+                }) : null}
+            </div>
+        </div>
+
+        const footer_subdomains = <div class="footer-column footer-subdomains">
+            <p class="footer-heading">Sub Domains</p>
+            <div class="footer-links">
+                {Object.keys(subdomains).length ? Object.keys(subdomains).map(name => {
+                    const link = subdomains[name];
+                    return <a class="footer-link" target="_blank" rel="noopener" title={name} href={link.url}>{name}</a>;
+                }) : null}
+            </div>
+        </div>
+
+        const footer_archives = <div class="footer-column footer-archives">
+            <p class="footer-heading">Archives</p>
+            <div class="footer-archive-list">
+                {archives && archives.length ? archives.map(item => (
+                    <span class="footer-archive-pill">
+                        <a class="footer-link" href={item.url}>{item.year}</a>
+                    </span>
+                )) : null}
+            </div>
+        </div>
+
+        return <> {svg_line}
+            <footer class="footer">
+                <div class="container">
+                    <div class="footer-grid">
+                        {footer_brand}
+                        {footer_subdomains}
+                        {footer_archives}
+                        {footer_social}
                     </div>
                 </div>
-            </div>
-        </footer>;
+            </footer></>;
     }
 }
 
