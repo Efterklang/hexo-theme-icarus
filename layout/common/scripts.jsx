0c4357c337f9ae93239db4ceaa25037a358e37f2
Efterklang <gaojiaxing2004@qq.com>
2025-12-13 19:48:41 +0800

    import script in scripts.jsx instead of layout.jsx

--- layout/common/scripts.jsx
+++ layout/common/scripts.jsx
@@ -11,6 +11,23 @@ module.exports = class extends Component {
             {config.comment?.js_url && <script defer src={config.comment.js_url}></script>}
             <Plugins site={site} config={config} page={page} helper={helper} head={false} />
             <script defer data-pjax src={url_for('/js/main.js')} ></script>
+            <script src="/js/host/medium-zoom/dist/medium-zoom.min.js" defer ></script>
+            <script src="/js/instant-page.min.js" type="module"> </script>
+            {config.plugins.live2d_Asoul && (
+                <>
+                    <script src="/js/live2d_Asoul/TweenLite.js" defer></script>
+                    <script
+                        src="/js/live2d_Asoul/live2dcubismcore.min.js"
+                        defer
+                    ></script>
+                    <script src="/js/live2d_Asoul/pixi.min.js" defer></script>
+                    <script src="/js/live2d_Asoul/cubism4.min.js" defer></script>
+                    <script src="/js/live2d_Asoul/pio.js" defer></script>
+                    <script src="/js/live2d_Asoul/pio_sdk4.js" defer></script>
+                    <script src="/js/live2d_Asoul/load.js" defer></script>
+                    <link href="/js/live2d_Asoul/pio.css" rel="stylesheet" type="text/css" />
+                </>
+            )}
         </Fragment>;
     }
 };
--- layout/layout.jsx
+++ layout/layout.jsx
@@ -53,71 +53,18 @@ module.exports = class extends Component {
             </div>
           </section>
           <div _ngcontent-gid-c24="" class="squiggle">
-            <svg
-              _ngcontent-gid-c24=""
-              aria-hidden="true"
-              width="100%"
-              height="8"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <pattern
-                _ngcontent-gid-c24=""
-                id="a"
-                width="91"
-                height="8"
-                patternUnits="userSpaceOnUse"
-              >
+            <svg _ngcontent-gid-c24="" aria-hidden="true" width="100%" height="8" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <pattern _ngcontent-gid-c24="" id="a" width="91" height="8" patternUnits="userSpaceOnUse" >
                 <g _ngcontent-gid-c24="" clip-path="url(#clip0_2426_11367)">
-                  <path
-                    _ngcontent-gid-c24=""
-                    d="M114 4c-5.067 4.667-10.133 4.667-15.2 0S88.667-.667 83.6 4 73.467 8.667 68.4 4 58.267-.667 53.2 4 43.067 8.667 38 4 27.867-.667 22.8 4 12.667 8.667 7.6 4-2.533-.667-7.6 4s-10.133 4.667-15.2 0S-32.933-.667-38 4s-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0"
-                    stroke="var(--lavender)"
-                    stroke-linecap="square"
-                  ></path>
+                  <path _ngcontent-gid-c24="" d="M114 4c-5.067 4.667-10.133 4.667-15.2 0S88.667-.667 83.6 4 73.467 8.667 68.4 4 58.267-.667 53.2 4 43.067 8.667 38 4 27.867-.667 22.8 4 12.667 8.667 7.6 4-2.533-.667-7.6 4s-10.133 4.667-15.2 0S-32.933-.667-38 4s-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0" stroke="var(--lavender)" stroke-linecap="square" ></path>
                 </g>
               </pattern>
-              <rect
-                _ngcontent-gid-c24=""
-                width="100%"
-                height="100%"
-                fill="url(#a)"
-              ></rect>
+              <rect _ngcontent-gid-c24="" width="100%" height="100%" fill="url(#a)" ></rect>
             </svg>
           </div>
           <Footer site={site} config={config} helper={helper} />
           <Scripts site={site} config={config} helper={helper} page={page} />
           <Search config={config} helper={helper} />
-
-          <script
-            type="text/javascript"
-            src="/js/imaegoo/imaegoo.js"
-            defer
-          ></script>
-          <script
-            src="/js/host/medium-zoom/dist/medium-zoom.min.js"
-            defer
-          ></script>
-          <script src="/js/instant-page.min.js" type="module"> </script>
-          {config.plugins.live2d_Asoul && (
-            <>
-              <script src="/js/live2d_Asoul/TweenLite.js" defer></script>
-              <script
-                src="/js/live2d_Asoul/live2dcubismcore.min.js"
-                defer
-              ></script>
-              <script src="/js/live2d_Asoul/pixi.min.js" defer></script>
-              <script src="/js/live2d_Asoul/cubism4.min.js" defer></script>
-              <script src="/js/live2d_Asoul/pio.js" defer></script>
-              <script src="/js/live2d_Asoul/pio_sdk4.js" defer></script>
-              <script src="/js/live2d_Asoul/load.js" defer></script>
-              <link
-                href="/js/live2d_Asoul/pio.css"
-                rel="stylesheet"
-                type="text/css"
-              />
-            </>
-          )}
         </body>
       </html>
     );
